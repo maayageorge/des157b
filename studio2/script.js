57ab54e83b627d8cbc44b6a7a05b8a9205288f65
Maaya <mvgeorge@ucdavis.edu>
2024-04-26 00:50:26 -0700

    studio2

--- studio2/script.js
+++ studio2/script.js
@@ -12,7 +12,8 @@
         document.querySelector('nav ul').innerHTML = createButtons(data);
 
         createEvents();
-        const initialDay = 'Monday'; // Adjust this to your initial day
+        
+        const initialDay = 'Monday'; 
         updateInterface(initialDay, globalData);
     }
 
@@ -23,6 +24,7 @@
         daysOfWeek.forEach(function(day){
             html += `<li><button id="${day}">${day}</button></li>`;
         });
+
         return html;
     }
 
@@ -33,22 +35,44 @@
             button.addEventListener('click', function(event) {
                 const day = event.target.id;
                 updateInterface(day, globalData);
+
+                const songTitle = document.querySelector('#song h2');
+                const img = document.querySelector('img');
+                const songlink = document.getElementById('songlink');
+
+                songTitle.classList.remove('hidden');
+                songTitle.classList.add('visible');
+                img.classList.remove('hidden');
+                img.classList.add('visible');
+
+                songlink.classList.remove('hidden');
+                songlink.classList.add('visible');
+   
             });
+
         }
+
     }
 
     function updateInterface(day, jsonData){
         console.log(jsonData);
         const songTitle = document.querySelector('#song h2');
         const img = document.querySelector('img');
-    
-        const { song, artist } = jsonData['Top Songs of the Week'][day];
+        const songlink = document.getElementById('songlink');
+
+
+        const { song, artist, link } = jsonData['Top Songs of the Week'][day];
         songTitle.innerHTML = `${song} <br> by ${artist}`;
     
         const imageName = day.toLowerCase();
         img.src = `images/${imageName}.jpg`; 
         img.alt = `${day} cover`;
+
+        songlink.href = link;
+        
     }
 
+
     getData();
+
 })();
